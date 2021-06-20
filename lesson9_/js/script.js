38f45447eb28fd493da40f07bb3ec2114aa8fcab
Eduardo Almeida Rodrigues <rod18027@byui.edu>
2021-06-19 22:54:22 -0300

    Implementation | Full lesson 9

---
+++ lesson9_/js/script.js
@@ -0,0 +1,49 @@
+const daylist = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const monthlist = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+var d = new Date();
+var day = d.getDay();
+var date = d.getDate();
+var month = d.getMonth();
+var year = d.getFullYear();
+
+var dateStr = daylist[day] + ", " + date + " " + monthlist[month] + " " + year;
+document.getElementById("dates").innerHTML = dateStr;
+document.getElementById("copyright").innerHTML = year;
+
+function toggleMenu() {
+  document
+    .getElementsByClassName("navigation")[0]
+    .classList.toggle("responsive");
+}
+
+if (daylist[day] == 'Friday') {  
+  document.getElementById("advertising").style.display = "initials";
+}
+else {
+  document.getElementById("advertising").style.display = "none";
+}
+
+function changeStormSeverity(rating) {
+  document.getElementById("stormvalue").innerHTML = rating;
+}
