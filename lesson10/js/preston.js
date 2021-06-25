52e17954ee5e2db02aa2173a836e07f2245ed315
Eduardo Almeida Rodrigues <rod18027@byui.edu>
2021-06-25 18:35:25 -0300

    Hot Fix | Summary Javascript not working online

--- lesson10/js/preston.js
+++ lesson10/js/preston.js
@@ -4,21 +4,6 @@ const apiKEY = "13742dc64c835fc2abcc75fb482f6836";
 const apiSummaryURL = "http://api.openweathermap.org/data/2.5/weather?id=" + cityID + "&units=" + unit + "&APPID=" + apiKEY;
 const apiForecastURL = "https://api.openweathermap.org/data/2.5/forecast?id=" + cityID + "&units=" + unit + "&APPID=" + apiKEY;
 
-/* Weather API Test */
-fetch(apiSummaryURL)
-  .then((response) => response.json())
-  .then((jsObject) => {
-    console.log(jsObject);
-
-    document.getElementById('current-temp').textContent = jsObject.main.temp;
-
-    const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
-    const desc = jsObject.weather[0].description;  // note how we reference the weather array
-    document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
-    document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
-    document.getElementById('icon').setAttribute('alt', desc);
-  });
-
 /* Summary Weather */
 fetch(apiSummaryURL)
   .then((response) => response.json())
